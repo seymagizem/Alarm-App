831494b28c6b049ad83a8bc8052bbcb0d49234e7
Gizem Sivri <104505014+seymagizem@users.noreply.github.com>
2023-03-12 21:24:16 +0300

    Update PropertyReflectable.swift

--- Alarm/PropertyReflectable.swift
+++ Alarm/PropertyReflectable.swift
@@ -1,10 +1,4 @@
-//
-//  PropertyReflectable.swift
-//  Alarm-ios-swift
-//
-//  Created by natsu1211 on 2017/01/30.
-//  Copyright © 2017年 LongGames. All rights reserved.
-//
+
 
 import Foundation
 
