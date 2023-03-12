10cbdba36f87132c3d014b44a097fa28a2d2633c
Gizem Sivri <104505014+seymagizem@users.noreply.github.com>
2023-03-12 21:25:49 +0300

    Update AlarmSchedulerDelegate.swift

--- Alarm/AlarmSchedulerDelegate.swift
+++ Alarm/AlarmSchedulerDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  AlarmSchedulerDelegate.swift
-//  Alarm-ios-swift
-//
-//  Created by natsu1211 on 2017/02/01.
-//  Copyright © 2017年 LongGames. All rights reserved.
-//
 
 import Foundation
 import UIKit
